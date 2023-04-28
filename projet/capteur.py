c58ec2d05111438a429f5d56bbf151dd40ca752c
Mouhamad Al Moustapha Mbaye <moustapha-mbaye@outlook.com>
2023-04-28 12:31:29 +0200

    capteur.py

--- projet/capteur.py
+++ projet/capteur.py
@@ -16,7 +16,7 @@ Squelette pour simuler un capteur qui est temporairement stocké sous la forme d
 
 """
     Dans ce fichier capteur.py, vous devez compléter les méthodes pour générer les données brutes vers Pandas 
-    et par la suite, les envoyer vers Kafka.
+    et par la suite, les envoyer vers Kafka. 
     ---
     Le premier devra prendre le contenue du topic donnees_capteurs pour le stocker dans un HDFS. (Fichier Kafka-topic.py)
     Le deuxième devra prendre le contenue du HDFS pour nettoyer les données que vous avez besoin avec SPARK, puis le stocker en HDFS.
@@ -35,73 +35,38 @@ def generate_dataFrame(col):
 def add_data(df: pd.DataFrame):
     """
     Cette méthode permet d'ajouter de la donnée vers votre DataFrame Pandas
+    # on va creer une boucle avec 10k itérations, vous devez créer et ajouter des données dans Pandas à valeur aléatoire
+    # Chaque itération comportera 1 ligne à ajouter. ex : timestamp = datetime.timedelta(seconds=1)
+    # on va creer une liste avec les elements créees
+    # ajouter une ligne au DataFrame
+    # retourner le dataframe
     """
-    # temps_execution =
-    current_timestamp = datetime.datetime.now()
-
-    
-    duration = 60 # Nombre de secondes pour la génération des données.
-
-    i = 0
-    while i < 1000:
-        ## Dans cette boucle, vous devez créer et ajouter des données dans Pandas à valeur aléatoire.
-        ## Chaque itération comportera 1 ligne à ajouter.
-        new_timestamp = current_timestamp + datetime.timedelta(seconds=1)
-        entrance_amount = np.random.randint(low=0, high=50)
-        exit_amount = np.random.randint(low=0, high=50)
-        temperature = np.random.normal(loc=20, scale=5)
-        humidity = np.random.normal(loc=50, scale=10)
-        parking_entrance = np.random.randint(low=1, high=5)
-        parking_exit = np.random.randint(low=1, high=5)
-        parking_actual_vehicle = np.random.randint(low=0, high=500)
-    
-        # ajouter une ligne au DataFrame
-        df.loc[i] = [new_timestamp, entrance_amount, exit_amount, temperature, humidity, parking_entrance, parking_exit, parking_actual_vehicle]
-        i += 1
+    percentage = 0
+    for i in range(10100):
+        timestamp = datetime.datetime.now() + datetime.timedelta(np.random.randint(low=1, high=120))
+        height = np.random.normal(loc=20, scale=5)
+        width = np.random.normal(loc=50, scale=10)
+        door_number = np.random.randint(low=1, high=7)
+        entrance_exit = bool(np.random.randint(0, 1))
+        parking_spot = np.random.randint(low=0, high=500)
+        df.loc[i] = [timestamp, height, width, door_number, entrance_exit, parking_spot]
+        if i and i % 1000 == 0:
+            percent = 100 - (10000//i)
+            if percent != percentage:
+                print("Data generation at {}%".format(percent))
+                percentage = percent
+            if i == 10000:
+                print("Data generation at 100%")
+                break
     return df
-
-
-def add_datatokafka():
-    """
-    Cette méthode permet d'ajouter de la donnée vers votre DataFrame Pandas
-    """
-    # temps_execution =
-    current_timestamp = datetime.datetime.now()
-    while 1:
-        ## Dans cette boucle, vous devez créer et ajouter des données dans Pandas à valeur aléatoire.
-        ## Chaque itération comportera 1 ligne à ajouter.
-        new_timestamp = current_timestamp + datetime.timedelta(seconds=1)
-        entrance_amount = np.random.randint(low=0, high=50)
-        exit_amount = np.random.randint(low=0, high=50)
-        temperature = np.random.normal(loc=20, scale=5)
-        humidity = np.random.normal(loc=50, scale=10)
-        parking_entrance = np.random.randint(low=1, high=5)
-        parking_exit = np.random.randint(low=1, high=5)
-        parking_actual_vehicle = np.random.randint(low=0, high=500)
-
-        liste = [new_timestamp.isoformat(), entrance_amount, exit_amount, temperature, humidity, parking_entrance, parking_exit,
-         parking_actual_vehicle]
-
-        # ajouter une ligne au DataFrame
-        producer = KafkaProducer( bootstrap_servers=['127.0.0.1:9092'],value_serializer=lambda x: json.dumps(x).encode('utf-8'))
-        producer.send("capteur", liste)
-
-
-
-def write_data_kafka(df: pd.DataFrame):
-    """
-    Cette méthode permet d'écrire le DataFrame vers Kafka.
-    (Optionnel)
-    """
-    csv_string = df.to_csv(index=False)
-    producer = KafkaProducer( bootstrap_servers=['127.0.0.1:9092'])
-    producer.send('nom_du_topic', value=bytes(csv_string, 'utf-8'))
-    producer.close()
 
 def write_data_minio(df: pd.DataFrame):
     """
     Cette méthode permet d'écrire le DataFrame vers Minio.
     (Obligatoire)
+    ## on va tester si la bucket existe , dans le cas contraire on la crer
+    ## on pousse le dataframe sur minio
+    #decommentez le code du dessous
     """
     client = Minio(
         "localhost:9000",
@@ -109,19 +74,20 @@ def write_data_minio(df: pd.DataFrame):
         access_key="minio",
         secret_key="minio123"
     )
-    found = client.bucket_exists("donnes-capteurs")
-    if not found:
-        client.make_bucket("donnes-capteurs")
+    if client.bucket_exists("receiver"):
+        client.make_bucket("receiver")
     else:
-        print("Bucket 'donnes-capteurs' existe déjà")
+        print("Bucket 'receiver' existe déjà")
 
     timestamp = datetime.datetime.now().strftime('%d-%m-%y')
-    df.to_csv("donnes_capteurs_" + str(timestamp) + ".csv", encoding='utf-8', index=False)
-    client.fput_object(
-        "donnes-capteurs", "donnes_capteurs_" + str(timestamp) + ".csv",  "donnes_capteurs_" + str(timestamp) + ".csv")
+    data = df.to_csv("receiver_" + str(timestamp) + ".csv", encoding='utf-8', index=False)
+    client.fput_object("receiver", "receiver_" + str(timestamp) + ".csv",  "receiver_" + str(timestamp) + ".csv")
 
 if __name__ == "__main__":
-    #columns = ["timestamp", "entrance_amount", "exit_amount", "temperature", "humidity", "parking_entrance", "parking_exit", "parking_actual_vehicle"]
-    #df = generate_dataFrame(columns)
-    #write_data_kafka(df)
-    #add_datatokafka()
+    """""
+    creer une liste column qui contient le header de votre dataframe 
+    decommentez le code du dessous
+    """""
+    columns = ['timestamp', 'height', 'width', 'door_number', 'entrance_exit', 'parking_spot']
+    df = generate_dataFrame(columns)
+    write_data_minio(df)
